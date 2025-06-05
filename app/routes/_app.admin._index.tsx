2bca57617357966794a0401b22a6a3ea0d99128f
Enpersonn <nicklas.b.overjordet@gmail.com>
2025-06-05 08:12:00 +0200

    made basic admin pages

--- app/routes/_app.admin._index.tsx
+++ app/routes/_app.admin._index.tsx
@@ -1,7 +1,65 @@
+import { PlusIcon } from "lucide-react";
+import {
+	Card,
+	CardTitle,
+	CardHeader,
+	CardContent,
+	CardFooter,
+} from "~/components/ui/card";
+import { Button } from "~/components/ui/button";
+import { Link } from "@remix-run/react";
+
 export default function AdminIndex() {
 	return (
-		<div>
-			<h1>Admin</h1>
+		<div className="grid grid-cols-2 gap-4">
+			<Card>
+				<CardHeader className="flex flex-row justify-between items-center mb-4">
+					<h2 className="text-xl font-semibold">Users</h2>
+					<Button asChild variant={"link"} className="px-0">
+						<Link to="/admin/users">Manage</Link>
+					</Button>
+				</CardHeader>
+				<CardContent className="flex justify-between items-center">
+					<p className="text-muted-foreground">Total Users:</p>
+					<p className="text-lg font-bold">56</p>
+				</CardContent>
+			</Card>
+			<Card>
+				<CardHeader className="flex flex-row justify-between items-center mb-4">
+					<h2 className="text-xl font-semibold">Parking Spots</h2>
+					<Button asChild variant={"link"} className="px-0">
+						<Link to="/admin/parkingspots">Manage</Link>
+					</Button>
+				</CardHeader>
+				<CardContent className="flex justify-between items-center">
+					<p className="text-muted-foreground">Total Parking Spots:</p>
+					<p className="text-lg font-bold">17</p>
+				</CardContent>
+			</Card>
+			<Card>
+				<CardHeader className="flex flex-row justify-between items-center mb-4">
+					<h2 className="text-xl font-semibold">Parking Requests</h2>
+					<Button asChild variant={"link"} className="px-0">
+						<Link to="/admin/parking_request">Manage</Link>
+					</Button>
+				</CardHeader>
+				<CardContent className="flex justify-between items-center">
+					<p className="text-muted-foreground">Total Active Parkings:</p>
+					<p className="text-lg font-bold">13</p>
+				</CardContent>
+			</Card>
+			<Card>
+				<CardHeader className="flex flex-row justify-between items-center mb-4">
+					<h2 className="text-xl font-semibold">Guests</h2>
+					<Button asChild variant={"link"} className="px-0">
+						<Link to="/admin/guests">Manage</Link>
+					</Button>
+				</CardHeader>
+				<CardContent className="flex justify-between items-center">
+					<p className="text-muted-foreground">Total Active Guests:</p>
+					<p className="text-lg font-bold">12</p>
+				</CardContent>
+			</Card>
 		</div>
 	);
 }
--- app/routes/_app.admin.tsx
+++ app/routes/_app.admin.tsx
@@ -1,5 +1,14 @@
 import type { LoaderFunctionArgs } from "@remix-run/node";
-import { Outlet, redirect, useLoaderData } from "@remix-run/react";
+import {
+	Link,
+	Outlet,
+	redirect,
+	useLoaderData,
+	useLocation,
+	useNavigate,
+} from "@remix-run/react";
+import { Button } from "~/components/ui/button";
+import { Card, CardHeader, CardContent, CardTitle } from "~/components/ui/card";
 import { getSupabaseServerClient } from "~/utils/supabase/supabase.server";
 
 export async function loader({ request }: LoaderFunctionArgs) {
@@ -24,7 +33,38 @@ export async function loader({ request }: LoaderFunctionArgs) {
 
 export default function Admin() {
 	const { user } = useLoaderData<typeof loader>();
-	return <Outlet />;
+	const location = useLocation();
+	return (
+		<div className="flex flex-col gap-5 h-screen w-screen items-start pt-10 justify-start max-w-5xl mx-auto px-4">
+			<header className="  w-full grid grid-cols-2 gap-4">
+				<Button
+					asChild
+					disabled={location.pathname === "/admin"}
+					variant={location.pathname === "/admin" ? "secondary" : "default"}
+				>
+					<Link to="/admin">
+						<p>Dashboard</p>
+					</Link>
+				</Button>
+				<Button asChild className="col-start-2">
+					<Link to="/">
+						<p>Back to app</p>
+					</Link>
+				</Button>
+			</header>
+			<Card className=" w-full">
+				<CardHeader className="flex flex-row justify-between items-center gap-4">
+					<CardTitle className="text-xl font-bold">Admin</CardTitle>
+					<Button asChild>
+						<Link to="/admin/logs">View logs</Link>
+					</Button>
+				</CardHeader>
+				<CardContent>
+					<Outlet />
+				</CardContent>
+			</Card>
+		</div>
+	);
 }
 
 export function ErrorBoundary({ error }: { error: Error }) {
